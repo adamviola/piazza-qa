2d2b02ba34cae28844c922e723cb3d4fa43286d3
Adam Viola <adambviola@gmail.com>
2020-03-12 14:34:07 -0400

    Added config.ini and logic for reading from it

---
+++ piazza_qa/piazza.py
@@ -0,0 +1,13 @@
+from piazza_api import Piazza
+
+
+def main():
+    p = Piazza()
+    p.user_login('', '')
+
+    profile = p.get_user_profile()
+    print(profile)
+
+    course = p.network('k7gqop42az52ck')
+
+    print(course.get_feed())
---
+++ start.py
@@ -0,0 +1,34 @@
+import os.path
+from configparser import ConfigParser, ParsingError
+from piazza_qa.piazza import main
+
+configuration_settings = {
+    'Account Settings': ['username', 'password', 'course_link'],
+    'Runtime Settings': ['check_interval']
+}
+
+# Check if configuration file exists
+if (not os.path.exists('config.ini')):
+    print('Configuration file \'config.ini\' is missing.')
+    exit()
+
+# Check for parsing errors
+try:
+    config = ConfigParser()
+    config.read('config.ini')
+except ParsingError as e:
+    print(str(e))
+    exit()
+
+# Verify that configuration file contains all settings to prevent rare errors
+for section in configuration_settings:
+    if not config.has_section(section):
+        print('Configuration file \'config.ini\' is missing section \'%s\'.' % section)
+        exit()
+
+    for option in configuration_settings[section]:
+        if not config.has_option(section, option):
+            print('Configuration file \'config.ini\' is missing option \'%s\' from section \'%s\'.' % (option, section))
+            exit()
+
+main()
